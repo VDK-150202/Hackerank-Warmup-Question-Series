c861b5f2ced63a01b911346f3fe3b3048bca4aef
Vaibhav Korlekar <65941094+VDK-150202@users.noreply.github.com>
2023-03-11 21:29:23 +0530

    Create 14-Hashing.java

---
+++ 14-Hashing.java
@@ -0,0 +1,26 @@
+
+
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+         int sum = 0;
+        int mlen = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        map.put(sum,-1);
+        for(int i=0;i<n;i++){
+            sum += arr[i];
+            if(map.containsKey(sum)==false){
+                map.put(sum,i);
+            }
+            else{
+                int len = i - map.get(sum);
+                if(len>mlen){
+                    mlen = len;
+                }
+            }
+        }
+        return mlen;
+        
+    }
+}
