44c3e7b80865d38a6adbdc2762b159729b0e2029
Vaibhav Korlekar <65941094+VDK-150202@users.noreply.github.com>
2023-03-19 20:12:47 +0530

    Update 17-Print-all-duplicates-in-input-String.java

--- 17-Print-all-duplicates-in-input-String.java
+++ 17-Print-all-duplicates-in-input-String.java
@@ -13,3 +13,20 @@ static void printDups(String str)
         System.out.println(mapElement.getKey() + ", count = " + mapElement.getValue());
     }
   }
+
+
+
+// cpp
+void printDups(string str)
+{
+    unordered_map<char, int> count;
+    for (int i = 0; i < str.length(); i++) {
+        count[str[i]]++;  //increase the count of characters by 1
+    }
+  
+    for (auto it : count) {   //iterating through the unordered map
+        if (it.second > 1)   //if the count of characters is greater than 1 then duplicate found
+            cout << it.first << ", count = " << it.second
+                 << "\n";
+    }
+}
