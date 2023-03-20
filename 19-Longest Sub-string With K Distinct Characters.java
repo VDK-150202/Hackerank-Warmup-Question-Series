5eed23d5691db6a3b0ad542074dd87497be9f484
Vaibhav Korlekar <65941094+VDK-150202@users.noreply.github.com>
2023-03-20 12:40:48 +0530

    Create 19-Longest Sub-string With K Distinct Characters.java

---
+++ 19-Longest Sub-string With K Distinct Characters.java
@@ -0,0 +1,37 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+	public static int getLengthofLongestSubstring(String s, int k) {
+		// Write your code here.
+		Map<Character, Integer> hm= new HashMap<>();
+		char arr[]= s.toCharArray();
+		int cc=0;
+		int max_count=Integer.MIN_VALUE;
+		int i=0, j=0;
+		while(i< arr.length){
+			hm.put(arr[i], hm.getOrDefault(arr[i], 0)+1);
+
+			if(hm.size()<= k){
+				for (Map.Entry<Character,Integer> mapElement : hm.entrySet()){
+					cc= cc+ mapElement.getValue();
+				}
+				max_count= Math.max(cc, max_count);
+				cc=0;
+			}
+
+			if(hm.size()>k){
+				while(hm.size()!=k){
+					hm.put(arr[j], hm.getOrDefault(arr[j], 0)-1);
+
+					if(hm.get(arr[j]) == 0 || hm.get(arr[j]) == -1){
+						hm.remove(arr[j]);
+					}
+					j++;
+				}
+			}
+			i++;
+		}
+		return max_count;
+
+	}
+}
